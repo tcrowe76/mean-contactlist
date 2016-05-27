901fbc497b452995acb0c319288adb21a5a9149e
tcrowe76 <tcrowe76@gmail.com>
2016-05-27 00:51:25 -0400

    working end to end

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -21,6 +21,34 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 			refresh();
 		});
 	};
+
+	$scope.remove = function(id) {
+		console.log(id);
+		$http.delete('/contactlist/' + id).success(function(response) {
+			console.log(response);
+			refresh();
+		});
+	};
+
+	$scope.edit = function(id) {
+		console.log(id);
+		$http.get('/contactlist/' + id).success(function(response) {
+			console.log(response);
+			$scope.contact = response;
+		})
+	};
+
+	$scope.update = function() {
+		console.log($scope.contact._id);
+		$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+			console.log(response);
+			refresh();
+		})
+	};
+
+	$scope.deselect = function() {
+		$scope.contact = "";
+	}
 	
 }]);
 
--- server.js
+++ server.js
@@ -22,7 +22,45 @@ app.post('/contactlist', function(req, res) {
 	db.contactlist.insert(req.body, function(err, doc) {
 		console.log(doc);
 		res.json(doc);
-	})
+	});
+});
+
+app.delete('/contactlist/:id', function(req, res) {
+	var id = req.params.id;
+	console.log('server received delete request for id ' + id);
+	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+		res.json(doc);
+	});
+});
+
+app.get('/contactlist/:id', function(req, res) {
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+		res.json(doc);
+	});
+});
+
+app.put('/contactlist/:id', function(req, res) {
+	var id = req.params.id;
+	console.log(req.body.name);
+	db.contactlist.findAndModify({
+		query: {
+			_id: mongojs.ObjectId(id)
+		},
+		update: {
+			$set: {
+				name: req.body.name,
+				email: req.body.email,
+				number: req.body.number
+			}
+		},
+		new: true
+	}, 
+	function(err, doc) {
+		res.json(doc);
+	}
+	);
 });
 
 app.listen(3000);
